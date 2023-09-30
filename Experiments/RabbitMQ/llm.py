4c974d9b03f0e238f8f6587415510fb4e5adff68
Muhammed Berkay Şan <78103617+BerkaySan@users.noreply.github.com>
2023-09-30 13:59:38 +0000

    Initial LLM Module Created

---
+++ Experiments/RabbitMQ/llm.py
@@ -0,0 +1,34 @@
+"""
+This module is used to get answer from LLM models you want. 
+
+keybard="bgimLv6dI06dIIsG1w0Bh1di7L6MgWHtqphm0XHSJMCMCs0FajFKF-ziGVyPYkcsFUAAEg."
+keygpt3="sk-15zYoWmkbwyTx3s4KODOT3BlbkFJeP3XstKOcNLWj8K5oyYNsk-15zYoWmkbwyTx3s4KODOT3BlbkFJeP3XstKOcNLWj8K5oyYN"
+"""
+from bardapi import Bard
+import openai
+class LLM:
+    def __init__(self, llm_model, api_key):
+        self.model_name = llm_model
+        self.api = api_key
+        if self.model_name not in ["bard", "gpt3"]:
+            raise Exception("Invalid LLM model")
+        if self.api == "" or self.api == None:
+            raise Exception("Invalid LLM api key")    
+
+
+    def get_answer(self, question):
+        if self.model_name == "bard":
+            self.model = Bard(self.api)
+            return self.model.get_answer(question)
+        elif self.model_name == "gpt3":
+            openai.api_key = self.api
+            response = openai.Completion.create(
+              model="gpt-3.5-turbo",
+              prompt="Q:"+question,
+              temperature=0.4,
+              stop=["\n"]
+            )
+            return response["choices"][0].text
+        else:
+            raise Exception("Invalid LLM model in getting answer.")
+    
---
+++ Experiments/RabbitMQ/tasks.py
@@ -0,0 +1,15 @@
+from celery import Celery 
+from time import sleep
+from llm import LLM
+
+app = Celery('tasks', broker='amqp://guest:guest@localhost', backend='redis://localhost')
+
+@app.task 
+def get_answer_from_llm(question):
+    sleep(30)
+    llm = LLM("bard","bgimLmavjMMRxUpfs_Hdy9DI31GegnmIzXLPwnxMxF3M_kL21AKzHs2OJarDpEhuXQa-Kg.")
+    return llm.get_answer(question)
+
+@app.task
+def exception_example():
+    raise Exception('An exception raised in a Celery task')
