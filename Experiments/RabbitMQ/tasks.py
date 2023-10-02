0e42d33ced518c832701bd88d5f19ed5460dd89e
Muhammed Berkay Şan <78103617+BerkaySan@users.noreply.github.com>
2023-10-02 19:00:58 +0000

    small changes

--- Experiments/RabbitMQ/tasks.py
+++ Experiments/RabbitMQ/tasks.py
@@ -2,14 +2,12 @@ from celery import Celery
 from time import sleep
 from llm import LLM
 
-app = Celery('tasks', broker='amqp://guest:guest@localhost', backend='redis://localhost')
+app = Celery('tasks', broker='amqp://guest:guest@rabbitmq', backend='redis://redis')
 
 @app.task 
-def get_answer_from_llm(question):
-    sleep(30)
-    llm = LLM("bard","bgimLmavjMMRxUpfs_Hdy9DI31GegnmIzXLPwnxMxF3M_kL21AKzHs2OJarDpEhuXQa-Kg.")
-    return llm.get_answer(question)
+def get_answer_from_llm(question,llm_model, llm_token):)
+    return LLM(llm_model,llm_token).get_answer(question)
 
 @app.task
-def exception_example():
+def exception():
     raise Exception('An exception raised in a Celery task')
