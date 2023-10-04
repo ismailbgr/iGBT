8d25abbd6212fa8bd4f4e14c8b6ff901fd25639c
Muhammed Berkay Şan <78103617+BerkaySan@users.noreply.github.com>
2023-10-04 15:47:14 +0300

    Update speech_texter.py

--- SpeechTexter/speech_texter.py
+++ SpeechTexter/speech_texter.py
@@ -1,8 +1,9 @@
 from google.cloud import speech_v1p1beta1 as speech
 import io
 import speech_recognition as sr
+from pydub import AudioSegment
 
-def speech_texter(json_path, input_file, output_file, language_code="en-US",model_type="local"):    
+def speech_to_text(json_path, input_file, output_file, language_code="en-US"):    
     # Replace 'your-credentials.json' with the path to your service account credentials JSON file.
     credentials_path = json_path
     client = speech.SpeechClient.from_service_account_json(credentials_path)
@@ -30,18 +31,28 @@ def speech_texter(json_path, input_file, output_file, language_code="en-US",mode
 
     with open(output_file, "w") as text_file:
         text_file.write(transcribed_text)
+    
 
-
-def speech_texter_local(input_file, output_file):
+def speech_to_text_local(input_file, output_file):
     # Replace 'input.mp3' with the path to your MP3 file.
-    input_file = 'input.mp3'
-    output_file = 'transcribed.txt'  # The name of the output text file
+
+    #input_file = 'input.mp3'
+    #output_file = 'transcribed.txt'  # The name of the output text file
 
     # Initialize the recognizer
     recognizer = sr.Recognizer()
+    def convert_mp3_to_wav(mp3_file, wav_file):
+        # Read the MP3 file
+        audio = AudioSegment.from_mp3(mp3_file)
 
+        # Export the audio to WAV format
+        audio.export(wav_file, format="wav")
+    # Usage example
+    mp3_file = "output.mp3"
+    wav_file = "audio.wav"
+    convert_mp3_to_wav(mp3_file, wav_file)
     # Load the audio file
-    with sr.AudioFile(input_file) as source:
+    with sr.AudioFile(wav_file) as source:
         audio_data = recognizer.record(source)
 
     # Use Google Web Speech API to transcribe the audio
@@ -54,10 +65,11 @@ def speech_texter_local(input_file, output_file):
             text_file.write(transcribed_text)
 
         print(f"Transcription completed. Text saved to {output_file}")
+        return transcribed_text
 
     except sr.UnknownValueError:
         print("Google Web Speech API could not understand the audio")
+        raise sr.UnknownValueError
     except sr.RequestError as e:
         print(f"Could not request results from Google Web Speech API; {e}")
-
-    
+        raise sr.RequestError
