4d03175c52e572f1faadec6c70c0fbc005640dbd
İsmail Buğra Öz <ozismailbugra@gmail.com>
2023-12-11 11:15:39 +0000

    added sharedData folder between services

--- SpeechTexter/tasks.py
+++ SpeechTexter/tasks.py
@@ -3,6 +3,7 @@ import base64
 import uuid
 import yaml
 from speech_texter import Speech2Text
+import os
 
 # Load config
 config = None
@@ -22,8 +23,30 @@ app = Celery(
 
 @app.task(name="speech2text")
 def speech2text(
+    input_file_path,
+    language_code="en-US",
+    model_name=config["speechtexter"]["model_name"],
+):
+    output_file = input_file_path + ".txt"
+
+    Speech2Text(
+        input_file_path, output_file, language_code, model_name
+    ).speech_to_text()
+
+    with open(output_file, "r") as f:
+        return f.read()
+
+
+@app.task(name="speech2text_file")
+def speech2text_file(
     input_file, language_code="en-US", model_name=config["speechtexter"]["model_name"]
 ):
+    """gets the text from the audio file from the base64 encoded mp3 file
+
+    Returns:
+        str: the text from the audio file
+    """
+
     input_file_name = str(uuid.uuid4()) + ".mp3"
     output_file = str(uuid.uuid4()) + ".txt"
 
