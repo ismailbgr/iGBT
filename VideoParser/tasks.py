e9dc36e679d9d89ea8db6698644533118afe1327
İsmail Buğra Öz <ozismailbugra@gmail.com>
2023-12-29 19:07:15 +0300

    Fix FFmpeg command failure and deprecate convert_video_to_mp3_file

--- VideoParser/tasks.py
+++ VideoParser/tasks.py
@@ -58,54 +58,12 @@ def convert_video_to_mp3(input_filepath):
 
     result = subprocess.run(command, check=False, capture_output=True)
     if result.returncode != 0:
+        current_task.update_state(state=states.FAILURE)
         raise Exception(f"FFmpeg command failed with error: {result.stderr.decode()}")
 
     print(f"Conversion completed. Output file: {output_file_path}")
 
     return output_file_path
-
-
-@app.task(name="convert_video_to_mp3_file")
-def convert_video_to_mp3(input_file):
-    """
-    DEPRECATED use convert_video_to_mp3 instead
-    Converts a given mp4 video file to an mp3 audio file.
-
-
-    Args:
-        input_file (str): Base64 encoded mp4 video file.
-
-    Returns:
-        b64 (str): The base64 encoded mp3 audio file.
-    """
-    current_task.update_state(state=states.STARTED)
-    uuidname = str(uuid.uuid4())
-    with open(f"/tmp/{uuidname}.mp4", "wb") as f:
-        f.write(base64.b64decode(input_file))
-    input_file = f"/tmp/{uuidname}.mp4"
-    output_file = f"/tmp/{uuidname}.mp3"
-
-    # Construct the FFmpeg command
-    command = [
-        "ffmpeg",
-        "-i",
-        input_file,
-        "-vn",
-        "-acodec",
-        "libmp3lame",
-        "-q:a",
-        "4",
-        "-y",
-        output_file,
-    ]
-    # Run the FFmpeg command
-    subprocess.run(command, check=True)
-    print(f"Conversion completed. Output file: {output_file}")
-
-    with open(output_file, "rb") as f:
-        data = f.read()
-        b64 = base64.b64encode(data)
-    return b64
 
 
 # youtube downloader
