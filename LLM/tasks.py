3190cc06ec62b39e4e677937b2252eb86e4b4c3b
gcosgun <gcosgun@etu.edu.tr>
2024-02-10 17:35:34 +0300

    minor fix for other llms

--- LLM/tasks.py
+++ LLM/tasks.py
@@ -26,9 +26,12 @@ print("llm initialized")
 
 
 @app.task(name="summarize")
-def summarize(text):
+def summarize(text, model_name='ollama', api_key=None):
     print("summarizing...")
     print("text: ", text)
+
+    llm = LLM(model_name, api_key)
+    
     # change task state to started
     try:
         current_task.update_state(state=states.STARTED)
